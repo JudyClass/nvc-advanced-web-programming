2a25867da8d7efc9dc1188def5033589337e98fa
Judy <jampafoo@gmail.com>
2018-01-24 19:56:41 -0600

    Submitting assignments

--- 02-Loops-Arrays-Functions/02-assignment.php
+++ 02-Loops-Arrays-Functions/02-assignment.php
@@ -19,6 +19,7 @@ function myFirstFunction() {
 
 myFirstFunction();
 
+
 // 02. Create a For Loop
 
 
@@ -33,20 +34,55 @@ myFirstFunction();
 
 // 04. Create an Array
 
-
-
+$cars = [
+	"Acura",
+	"Ford",
+	"Toyota"
+];
+print_r($cars);
 
 
 // 05. Sort an Array
 
 
+$selectedCar = $cars[rand(0,2)];
 
-
+$countCars = count($cars);
+for($x = 0; $x < $countCars; $x++) {
+    echo $cars[$x];
+    echo "<br>";
+}
 
 // 01. Create a Switch Statement
 
+$title = null;
 
-
+switch  (date('h')) {
+	case 01:
+	  $title = "NVC Early";
+	  break;
+	case 05:
+	  $title = "NVC Breakfast";
+	  break;
+	case 07:
+		$title = "NVC"; 
+		break;
+	default: 
+		$title = "Northwest Vista College";
+}
 
 
 ?>
+
+<html>
+	<head>
+		<title><?php echo $title; ?></title>
+	</head>
+	<body>
+		<h1><?php echo $title; ?></h1>
+		<p><?php echo "It is " . date('h') . " hours"; ?></p>
+		<p><?php echo "You won a new " . $selectedCar . "!"?></p>
+		<p><?php echo "Alpha sorting " . $alphaCarOrder ?></p>
+
+	</body>
+</html>
