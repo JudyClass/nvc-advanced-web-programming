06b2d88f3d426b693d6d20dec6cdd3a9a35aab0a
Judy <jampafoo@gmail.com>
2018-02-05 00:10:42 -0600

    updating assignment

--- 02-Loops-Arrays-Functions/02-assignment.php
+++ 02-Loops-Arrays-Functions/02-assignment.php
@@ -11,28 +11,41 @@
 
 // 01. Create and Call at least one function.
 
-$yas= "Awesome sauce";
 
-function myFirstFunction() {
-	echo $yas;
+
+function myFirstFunction($yas= "Awesome sauce") {
+	echo "Things are $yas. <br>";
 }
 
-myFirstFunction();
+echo "<h2>These are function calls</h2>";
+myFirstFunction(); 
+myFirstFunction('ok');
+
+
+
 
 
 // 02. Create a For Loop
+
+echo "<h2>This is a loop</h2>";
+for($i=1; $i<=15; $i++){
+    echo "The number is " . $i . "<br>";
+}
 
 
 
 
 
 // 03. Create a While Loop
+echo "<h2>This is a while loop</h2>";
+
 
 
 
 
 
 // 04. Create an Array
+echo "<h2>This is an array</h2>";
 
 $cars = [
 	"Acura",
@@ -42,34 +55,54 @@ $cars = [
 print_r($cars);
 
 
+
+
+
 // 05. Sort an Array
+echo "<h2>This is some array sorting.</h2>";
 
-
+//min,max (because there are only 3 cars in my array)
 $selectedCar = $cars[rand(0,2)];
 
-$countCars = count($cars);
-for($x = 0; $x < $countCars; $x++) {
-    echo $cars[$x];
-    echo "<br>";
-}
+echo "You won a new " . $selectedCar . "!";
+
+
+
+
+
 
 // 01. Create a Switch Statement
+echo "<h2>This is a switch statement</h2>";
+
+date_default_timezone_set("America/Chicago");
 
 $title = null;
 
-switch  (date('h')) {
+switch  (date('H')) {
 	case 01:
-	  $title = "NVC Early";
+	  $title = "TOO Early";
 	  break;
 	case 05:
-	  $title = "NVC Breakfast";
+	  $title = "WAKE UP";
 	  break;
 	case 07:
-		$title = "NVC"; 
+		$title = "Good Morning"; 
 		break;
+	case 16:
+		$title = "Evening Time"; 
+		break;
+	case 17:
+		$title = "Evening Time"; 
+		break;
+	case 18:
+		$title = "Night Time"; 
+		break;			
 	default: 
-		$title = "Northwest Vista College";
+		$title = "a Good Day";
 }
+
+echo date('h:i:s');
+echo "It is $title !"; 
 
 
 ?>
@@ -79,10 +112,8 @@ switch  (date('h')) {
 		<title><?php echo $title; ?></title>
 	</head>
 	<body>
-		<h1><?php echo $title; ?></h1>
-		<p><?php echo "It is " . date('h') . " hours"; ?></p>
-		<p><?php echo "You won a new " . $selectedCar . "!"?></p>
-		<p><?php echo "Alpha sorting " . $alphaCarOrder ?></p>
-
+		
+     <?php // Nothing in the body yet! ?>
+ 
 	</body>
 </html>
